2b975fead340d11206917775b28f56f26a934380
Jeremy Bi <bixuanxbi@gmail.com>
2014-03-19 21:19:59 +0800

    Finish dividing step 3

--- src/main/scala/Exercise.scala
+++ src/main/scala/Exercise.scala
@@ -5,6 +5,8 @@ import org.apache.spark.SparkContext._
 object SimpleExercise {
 
   def main(args: Array[String]) = {
+
+    // Initialization
     val conf = new SparkConf()
       .setMaster("local")
       .setAppName("Exercise")
@@ -13,53 +15,106 @@ object SimpleExercise {
     val sc = new SparkContext(conf)
 
 
-
+    // Step 1
     // Divide by predicate
     val file1 = sc.textFile("/Users/jeremybi/Desktop/standard_data/output0_0")
-    
+      
     // predicatePair is RDD[(predicate, [(subject, object)])]
     val predicatePair = file1.map(line => line.split("&")).
       map(triple => {
-            val regex = ".*#([a-zA-Z]+)>".r
+            val Regex = ".*#([a-zA-Z]+)>".r
             val predicate = triple(1) match {
-              case regex(pred) => pred
+              case Regex(pred) => pred
               case _ => "noMatch"
             }
 
             (predicate, (triple(0), triple(2)))
-          
-          }).groupByKey
-
-    predicatePair.collect.foreach(pair => writeToHDFS(sc, pair, ""))
+              
+          }).groupByKey.cache
 
 
-    // Divide by object in type predicate
-
-    // objectPair is [(subject, object)]
-    val objectPair = predicatePair.
+    // Step 2
+    // Subdivide by object in type predicate
+    // classPair is Map[class, Seq(class, object)]
+    val classPair = predicatePair.
       filter(pair => pair._1 == "type").
       first._2.
       map(truple => {
-            val regex2 = "<.*#([a-zA-Z]+)>$".r
-            val subName =  truple._2 match {
-              case regex2(subtype) => subtype
+            val Regex2 = "<.*#([a-zA-Z]+)>$".r
+            val objName =  truple._2 match {
+              case Regex2(subtype) => subtype
               case _ => "noMatch"
             }
 
-            (subName, truple._1)
-          })
+            (objName, truple._1)
+          }).groupBy(_._1)
 
-    // subjectPair is RDD[(subject, [object])]
-    val subjectPair = sc.parallelize(objectPair).groupByKey
+    classPair.foreach(pair => writeToHDFS(sc, pair._2, "type/" ++ pair._1))
 
-    subjectPair.collect.foreach(pair => writeToHDFS(sc, pair, "type/"))
+
+    // Step 3
+    // Subdivide all predicates other than type predicate
+
+    // by object
+    val split1 = predicatePair.filter(pair => pair._1 != "type").
+      map(pair =>
+        (pair._1, pair._2.
+           map(tuple => (findClass(tuple._2, classPair), tuple)).
+           groupBy(_._1)))
+
+    split1.collect.
+      foreach(pair =>   // (predicate, Map[class, Seq[(class, (s,o))]])
+        pair._2.foreach(tuple => // (class, Seq[(class, (s,o))])
+          writeToHDFS(sc, tuple._2, pair._1 ++ "/" ++ pair._1 ++ "_" ++ tuple._1)
+        )
+      )
+
+    // by subject
+    val split2 = predicatePair.filter(pair => pair._1 != "type").
+      map(pair =>
+        (pair._1, pair._2.
+           map(tuple => (findClass(tuple._1, classPair), tuple)).
+           groupBy(_._1)))
+
+    split2.collect.
+      foreach(pair =>   // (predicate, Map[class, Seq[(class, (s,o))]])
+        pair._2.foreach(tuple => // (class, Seq[(class, (s,o))])
+          writeToHDFS(sc, tuple._2, pair._1 ++ "/" ++ tuple._1 ++ "_" ++ pair._1)
+        )
+      )
+
+    // by subject and object
+    val split3 = predicatePair.filter(pair => pair._1 != "type").
+      map(pair =>
+        (pair._1, pair._2.
+           map(tuple =>
+             ((findClass(tuple._1, classPair), findClass(tuple._2, classPair)),
+              tuple)).
+           groupBy(_._1)))
+
+    split3.collect.
+      foreach(pair => // (predicate, Map[(class, class), Seq[((class,class), (s,o))]])
+        pair._2.foreach(tuple => // ((class, class), Seq[((class,class), (s,o))])
+          writeToHDFS(sc, tuple._2,
+                      pair._1 ++ "/" ++ tuple._1._1 ++
+                      "_" ++ pair._1 ++ "_" ++ tuple._1._2)
+        )
+      )
+    
 
     sc.stop()
   }
 
-  def writeToHDFS(sc: SparkContext, pair: (String, Seq[AnyRef]), path: String) = {
-    sc.parallelize(pair._2).
-      saveAsTextFile("hdfs://localhost:9000/user/jeremybi/" ++ path ++ pair._1)
+  def writeToHDFS(sc: SparkContext, seq: Seq[AnyRef], path: String) = {
+    sc.parallelize(seq).
+      saveAsTextFile("hdfs://localhost:9000/user/jeremybi/" ++ path)
   }
-  
+
+  def findClass(obj: String, map: Map[String, Seq[(String, String)]]) =
+    map.foldLeft("noMatch")((flag, pair) =>
+      if (pair._2 contains (pair._1, obj))
+        pair._1
+      else flag
+    )
+
 }
